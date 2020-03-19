e64d26ddc3282cff6bd38fa4565adcb5e60cb947
arhi <vadik_mig@mail.ru>
2020-03-19 11:26:03 +0300

    update

--- js/players.js
+++ js/players.js
@@ -1,5 +1,5 @@
 import obj from '../data/games.json';
-let allGames = 447;
+let allGames = 452;
 let ratingPlayers = [];
 let nonRatingPlayers = [];
 let redWins = 0;
@@ -57,6 +57,7 @@ playerRow("Цензор");
 playerRow("Печкин");
 playerRow("Enigma");
 playerRow("Игрок9");
+playerRow("Улитко");
 
 export function playerRow(nick, num) {
     let player = {};
